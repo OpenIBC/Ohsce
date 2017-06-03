b875847fc5829a317f60e9ab0af1b02aa0f82bca
SuiFengXingHai <lyztj100@163.com>
2017-06-03 21:22:10 +0800

    V0.2.0.2
    V0.2.0.2

--- OHSceRun.php
+++ OHSceRun.php
@@ -1,12 +1,12 @@
 <?php
 /*
-OHSCE_V0.2.0_B
+OHSCE_V0.2.0.2_B
 高可靠性的PHP通信框架。
 HTTP://WWW.OHSCE.ORG
 @作者:林友哲 393562235@QQ.COM
 作者保留全部权利，请依照授权协议使用。
 */
-ini_set('memory_limit',"88M");//重置php可以使用的内存大小为64M
+ini_set('memory_limit',"128M");//重置php可以使用的内存大小为128M
 set_time_limit(0);
 ob_implicit_flush(1);
 include('loadohsce.php');
