96e5148fcabbcf01b7f924bc9d537fc26c38018b
LinYouzhe <lyztj100@163.com>
2017-06-05 00:16:42 +0800

    V0.2.0.2
    V0.2.0.2

--- config/oibc_sce_config.php
+++ config/oibc_sce_config.php
@@ -7,16 +7,16 @@ HTTP://WWW.OHSCE.ORG
 作者保留全部权利，请依照授权协议使用。
 该文件禁止改名!否则可能会无法运行！
 */
-define('OHSCE_MYIP', '127.0.0.1');//本机IP地址
-define('OHSCE_MYIP_SYSTEM', '127.0.0.1');//本机IP地址（系统内）
-define('OHSCE_PHPDIR', 'php');//您的PHP所在路径
+define('OHSCE_MYIP', '127.0.0.1');//本机IP地址   实际的用户网地址
+define('OHSCE_MYIP_SYSTEM', '127.0.0.1');//本机IP地址（系统内）   实际的设备网地址
+define('OHSCE_PHPDIR', 'php');//您的PHP所在路径  如c:/phpdir/php.exe
 define('OHSCE_OLMD_MADDRESS',"20");//OLMD内存起始
 define('OHSCE_OLMD_MADDRESSBACKUPS',"10");//OLMD内存起始（备）
 define('OHSCE_OLMD_MADDRESSPORT',"7698");//OLMD监听端口
 define('OHSCE_OLMD_MADDRESSPORTY',"7699");//OLMD监听端口(对外读取)
 define('OHSCE_OLMD_MADDRESSPASS',"ohsceolmdpassword"); //OLMD写入密钥，如果你不善于维护防火墙，那么你必须修改它
 define('OHSCE_OLMD_MADDRESSPASSY',"password");//这是OLMD对外读取密钥，如果你不善于维护防火墙，那么你必须修改它
-//define('OHSCE_ROOTDIR',dirname(__FILE__));
+//define('OHSCE_ROOTDIR',‘’);
 $OHSCE_PLdir=OHSCE_ROOTDIR.'/PcenterRun/';//PCENTER直启目录
 $OHSCE_pdefend=OHSCE_ROOTDIR.'/OHSceRun.php -r engine -m pdefend -p';
 $OHSCE_pdefend_vbs=OHSCE_ROOTDIR.'/pdefend.vbs';
